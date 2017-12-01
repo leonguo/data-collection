6733645d5ab1f0b2186d7ab3c6b834895e54b77c
秩序 <hotman8168@gmail.com>
2017-12-01 16:12:54 +0800

    cninfo index 抓取

--- cninfo/cninfo/spiders/index_spider.py
+++ cninfo/cninfo/spiders/index_spider.py
@@ -1,19 +1,16 @@
 import scrapy
-
+import json
 
 class IndexSpider(scrapy.Spider):
     name = "index"
 
     def start_requests(self):
         urls = [
-            'http://www.cninfo.com.cn/cninfo-new/index',
+            'http://www.cninfo.com.cn/cninfo-new/disclosure/szse_latest',
+            'http://www.cninfo.com.cn/cninfo-new/disclosure/sse_latest',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = "index"
-        filename = 'cninfo-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        self.logger.info("response: poster index page[%s] crawl status: %d", response.url, response.status)
