ad835e4515d8c2f1523d91d0ed4bb5570d2a038c
wings <hotman8168@gmail.com>
2017-12-03 23:57:53 +0800

    del

--- cninfo/cninfo/spiders/index_spider.py
+++ cninfo/cninfo/spiders/index_spider.py
@@ -15,11 +15,12 @@ class IndexSpider(scrapy.Spider):
         ]
         today = datetime.date.today()
         for url in urls:
-            yield scrapy.FormRequest(url=url, callback=self.parse, formdata={"seDate": "2017-12-01","tabName":"fulltext"})
+            yield scrapy.FormRequest(url=url, callback=self.parse,
+                                     formdata={"seDate": "2017-12-01", "tabName": "fulltext"})
 
     def parse(self, response):
         logger = logging.getLogger()
         logger.info("response: poster index page[%s] crawl status: %d", response.url, response.status)
-        logger.info(unicode(response.body,"utf-8"))
+        logger.info(unicode(response.body, "utf-8"))
         # jsonBody = json.loads(response.body)
-        # print  json.dumps(jsonBody)
+        # print  json.dumps(jsonBody)0.
