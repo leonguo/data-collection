e26903f300df35de52093b3a8c69c52e0b4cd862
秩序 <hotman8168@gmail.com>
2017-12-08 17:21:39 +0800

    crawl index code

--- cninfo/cninfo/pipelines.py
+++ cninfo/cninfo/pipelines.py
@@ -5,10 +5,12 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import psycopg2
+import logging
 
 class CodePipeline(object):
     def __init__(self, psql_dsn):
         self.psql_dsn = psql_dsn
+        self.logger = logging.getLogger()
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -18,12 +20,22 @@ class CodePipeline(object):
 
     def open_spider(self, spider):
         self.client = psycopg2.connect(self.psql_dsn)
+        self.cur = self.client.cursor()
 
     def close_spider(self, spider):
+        self.cur.close()
         self.client.close()
 
     def process_item(self, item, spider):
-        print "ddd"
+        sec_code = int(item["sec_code"])
+        sec_name = item["sec_name"]
+        insert_q = "INSERT INTO share_certificate(sec_code,sec_name) VALUES ({0},'{1}') ON CONFLICT DO NOTHING;".format(sec_code,sec_name)
+        self.logger.warning(unicode(insert_q, "utf-8"))
+        try:
+            self.cur.execute(insert_q)
+        except psycopg2.Error:
+            self.logger.exception('Database error')
+        self.client.commit()
         return item
     #
     # def process_item(self, item, spider):
--- cninfo/cninfo/spiders/code_spider.py
+++ cninfo/cninfo/spiders/code_spider.py
@@ -7,6 +7,11 @@ from datetime import date, datetime
 
 from ..items import CodeItem
 from scrapy.loader import ItemLoader
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 
 class CodeSpider(scrapy.Spider):
     name = "code"
@@ -24,10 +29,10 @@ class CodeSpider(scrapy.Spider):
 
     def parse(self, response):
         self.logger.warning("response: poster index page[%s] crawl status: %d", response.url, response.status)
-        l = ItemLoader(item=CodeItem(), response=response)
-        l.add_xpath('name', '//div[@class="product_name"]')
-        l.add_xpath('name', '//div[@class="product_title"]')
-        l.add_xpath('price', '//p[@id="price"]')
-        # code_item['sec_code'] = 1000
-        # code_item['sec_name'] = "ddd"
-        return l.load_item()
+        code_table = response.xpath('//table[@class="table_data"]/tr')
+        for code in code_table:
+            l = ItemLoader(item=CodeItem(), selector=code)
+            l.default_output_processor = scrapy.loader.processors.TakeFirst()
+            l.add_xpath('sec_code', 'td[1]/nobr/a/text()')
+            l.add_xpath('sec_name', 'td[2]/nobr/a/text()')
+            yield l.load_item()
