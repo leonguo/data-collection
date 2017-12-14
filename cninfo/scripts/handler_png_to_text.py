9cca3d2cdc42f4e593eabe97c9ceb05335758d7a
秩序 <hotman8168@gmail.com>
2017-12-14 11:15:13 +0800

    crawl film

--- cninfo/scripts/handler_png_to_text.py
+++ cninfo/scripts/handler_png_to_text.py
@@ -6,11 +6,10 @@ import pytesseract
 import requests
 from io import BytesIO
 
+tessdata_dir_config = '--tessdata-dir "C:/Program Files (x86)/Tesseract-OCR/"'
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
 
 img_src = "http://img.58921.com/sites/all/movie/files/protec/eeba4743c229d809b3205de018a60ee6.png"
 response = requests.get(img_src)
-print response.content
 img = Image.open(BytesIO(response.content))
-print img
-print(pytesseract.image_to_string(img))
+print(pytesseract.image_to_string(img, lang='chi_sim', config=tessdata_dir_config))
