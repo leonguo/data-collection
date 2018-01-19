03f4d10f2fe74a749e21305820a2585f0a6c1964
秩序 <hotman8168@gmail.com>
2018-01-19 11:35:34 +0800

    image update

--- cninfo/scripts/pinterest.py
+++ cninfo/scripts/pinterest.py
@@ -3,6 +3,7 @@
 from selenium import webdriver
 import time
 
+
 def main():
     # You most certainly want to change this
     url = "https://www.pinterest.com/login/?referrer=home_page"
@@ -10,7 +11,7 @@ def main():
     profile = webdriver.FirefoxProfile()
     profile.set_preference("general.useragent.override",
                            "Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 selenium.py")
-    browser = webdriver.Firefox(profile,executable_path='E:\js\geckodriver.exe')
+    browser = webdriver.Firefox(profile, executable_path='E:\js\geckodriver.exe')
     browser.get(url)
     time.sleep(1)
 
@@ -24,16 +25,16 @@ def main():
     user.send_keys("hotman8168@gmail.com")
     password.send_keys("hotman8168com")
     time.sleep(1)
-    browser.find_elements_by_xpath("input[@type='submit']").click()
+    browser.find_element_by_css_selector('button.red.SignupButton.active').click()
 
     # Keep the page loaded for 8 seconds
     time.sleep(5)
 
     print(browser.current_url)
-    print(browser.find_element_by_id('content').text)
-
+    browser.get(browser.current_url)
+    content = browser.page_source
+    print content
     browser.close()
-
 
 if __name__ == '__main__':
     main()
